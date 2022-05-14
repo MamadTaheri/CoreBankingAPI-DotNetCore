3d23d302005eeac33662b9acf18ad4e5af2e402a
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-14 23:14:55 +0430

    add Account Model

---
+++ CoreBanking/Src/CoreBanking.API/Models/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CoreBanking.API.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AccountName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public decimal CurrentAccountBalance { get; set; }
+        public AccountType AccountType { get; set; }
+        public string AccountNumberGenerated { get; set; }
+        public byte[] PinHash { get; set; }
+        public byte[] PinSalt { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateLastUpdated { get; set; }
+    }
+
+    public enum AccountType
+    {
+        Savings,
+        Current,
+        Corporate,
+        Government
+    }
+}

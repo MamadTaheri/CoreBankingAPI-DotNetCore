909c727d90bd0372ad791e5b9a79c218764fb9f8
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-15 00:52:37 +0430

    create IAccountService Interface

---
+++ CoreBanking/Src/CoreBanking.API/Services/Interfaces/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿using CoreBanking.API.Models;
+using System.Collections.Generic;
+
+namespace CoreBanking.API.Services.Interfaces
+{
+    public interface IAccountService
+    {
+        Account Authenticate(string AccountNumber, string pin);
+        IEnumerable<Account> GetAllAccounts();
+        Account Create(Account account, string pin, string ConfirmPin);
+        void Update(Account account, string pin = null);
+        void Delete(int Id);
+        Account GetById(int Id);
+        Account GetByAccountNumber(string Accountnumber);
+    }
+}

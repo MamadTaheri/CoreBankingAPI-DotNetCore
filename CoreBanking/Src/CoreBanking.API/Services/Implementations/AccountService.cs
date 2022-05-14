d0606584753504604b5abe0760ba99259d404878
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-15 01:15:48 +0430

    create AccountService

---
+++ CoreBanking/Src/CoreBanking.API/Services/Implementations/AccountService.cs
@@ -0,0 +1,52 @@
+﻿using CoreBanking.API.DAL;
+using CoreBanking.API.Models;
+using CoreBanking.API.Services.Interfaces;
+using System.Collections.Generic;
+
+namespace CoreBanking.API.Services.Implementations
+{
+    public class AccountService : IAccountService
+    {
+        // Dependency Injection in Class Constructor
+        // تزریق وابستگی درون سازنده کلاس 
+        public AccountService()
+        {
+
+        }
+       
+        public Account Authenticate(string AccountNumber, string pin)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Account Create(Account account, string pin, string ConfirmPin)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Account> GetAllAccounts()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Account GetByAccountNumber(string Accountnumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Account GetById(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Account account, string pin = null)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

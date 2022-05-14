3a6d572cde081e8b11dca0aa5d10103b13da9cc1
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-15 00:52:25 +0430

    create ITransactionService Interface

---
+++ CoreBanking/Src/CoreBanking.API/Services/Interfaces/ITransactionService.cs
@@ -0,0 +1,14 @@
+﻿using CoreBanking.API.Models;
+using System;
+
+namespace CoreBanking.API.Services.Interfaces
+{
+    public interface ITransactionService
+    {
+        Response CreateNewTransaction(Transaction transaction);
+        Response FindTransactionByDate(DateTime date);
+        Response MakeDeposit(string AccountNumber, decimal Amount, string TransactionPin);
+        Response MakeWithDrawal(string AccountNumber, decimal Amount, string TransactionPin);
+        Response MakeFundsTransfer(string FromAccount, string ToAccount, decimal Amount, string TransactionPin);
+    }
+}

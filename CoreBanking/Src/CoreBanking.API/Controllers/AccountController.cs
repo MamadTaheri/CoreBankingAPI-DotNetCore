00b5608694e28ae8d6cfae6f429d14b7746e4868
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-16 00:48:10 +0430

    add AccountController

---
+++ CoreBanking/Src/CoreBanking.API/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using CoreBanking.API.Models;
+using CoreBanking.API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreBanking.API.Controllers
+{
+    [ApiController]
+    [Route("api/v3/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private IAccountService _accountService;
+        IMapper _mapper;
+        public AccountController(IAccountService accountService, IMapper mapper)
+        {
+            _accountService = accountService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("register_new_account")]
+        public IActionResult RegisterNewAccount([FromBody] RegisterNewAccountModel newAccount)
+        {
+            if(!ModelState.IsValid) return BadRequest(newAccount);
+
+            // map to account
+            var account = _mapper.Map<Account>(newAccount);
+            return Ok(_accountService.Create(account, newAccount.Pin, newAccount.ConfirmPin));
+        }
+
+        [HttpGet]
+        [Route("get_all_accounts")]
+        public IActionResult GetAllAccounts()
+        {
+            // map Accunt Model to GetAccountModel
+            var accounts = _accountService.GetAllAccounts();
+            var cleanedAccounts = _mapper.Map<IList<GetAccountModel>>(accounts);
+            return Ok(cleanedAccounts);
+        }
+
+    }
+}

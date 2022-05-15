701e625f5f8ddd739316f4ed7097f99874773b8e
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-16 00:24:00 +0430

    add Mapeer

---
+++ CoreBanking/Src/CoreBanking.API/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CoreBanking.API.Models;
+
+namespace CoreBanking.API.Profiles
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<RegisterNewAccountModel, Account>();
+            CreateMap<UpdateAccountModel, Account>();
+            CreateMap<Account, GetAccountModel>();
+        }
+    }
+}
